4f224f4a230c1fa4001e063f0f13c13f58cfd01d
Arsenii Zima <arseniimac12@Arseniis-MacBook.local>
2019-10-28 01:09:49 +0200

    fix 4 task

--- scriptFourth.js
+++ scriptFourth.js
@@ -1,14 +1,15 @@
-var numberArray = [40, 1, 5, 200, 400, 0, -1];
-function compareNumbers(a, b) {
-    setTimeout(compareNumbers, 3000);//need fix
-    return a - b;
-}
-
-console.log('Fourth task: ', numberArray.sort(compareNumbers));
-/*
-var newArray = numberArray.sort(compareNumbers);
-function intervalWait(a, b){
-    setInterval( compareNumbers, 3000);
-}
-console.log('Fourth task 3second: ', newArray.sort(intervalWait));
-//console.log('Fourth task:',aclean(array) );*/
+ (function() {
+    var numberArray = [40, 1, 5, 200, 400, 0, -1];
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    var arr = numberArray.sort(compareNumbers);
+                i = -1;
+            return function stepByStep() {
+                if ( ++i < arr.length ) {
+                    console.log('Fourth task',arr[i]);
+                    document.querySelector('#task4').innerHTML += arr[ i ] +'<br/>';
+                    setTimeout(stepByStep, 3000 );
+                }
+            }();
+        })();
